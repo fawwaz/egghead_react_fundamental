3e162e0714b3a42faaed86e72434a7f1f1d0ede7
fawwazmuhammad <fawwazmuhammad@gmail.com>
2016-05-29 16:05:46 +0700

    Step 02: Hello World - first Component

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+
+// Class component can have state
 class App extends React.Component {
   render(){
-    return <div>Hi</div>
+  	//return React.createElement('h1',null,'Hello guys !')
+    return <div><h1>Hello World</h1></div>
   }
 }
 
+// Stateless function will not have state 
+// const App = () => <h1>Hello Eggheads</h1>
 export default App
