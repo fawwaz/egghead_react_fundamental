1d8a0c3885889e0a6294b088f7f4a3e49e4bfed5
fawwazmuhammad <fawwazmuhammad@gmail.com>
2016-05-29 16:09:14 +0700

    Step 03 : The render method

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import React from 'react';
 // Class component can have state
 class App extends React.Component {
   render(){
-  	//return React.createElement('h1',null,'Hello guys !')
-    return <div><h1>Hello World</h1></div>
+    return (
+    	<div>
+    		<h1> Hello World</h1>
+    		<b> Bold </b>
+    	</div>
+    );
   }
 }
 
-// Stateless function will not have state 
-// const App = () => <h1>Hello Eggheads</h1>
 export default App
