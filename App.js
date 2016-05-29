a949863f5c689603a759afef545f3c1399311279
fawwazmuhammad <fawwazmuhammad@gmail.com>
2016-05-29 16:25:22 +0700

    step 05 : State basics

--- App.js
+++ App.js
@@ -3,26 +3,28 @@ import ReactDOM from 'react-dom';
 
 // Class component can have state
 class App extends React.Component {
+  constructor(){
+  	super();
+  	this.state = {
+  		txt : 'this is the state',
+  		cat : 0
+  	}
+  }
+  update(e){
+  	this.setState({txt:e.target.value})
+  }
   render(){
   	let txt = this.props.txt
     return (
     	<div>
-    		<h1> {this.props.txt}</h1>
-    		<h1> {txt}</h1>
+    		<input type="text" onChange={this.update.bind(this)} />
+    		<h2> {this.state.txt} </h2>
     	</div>
     );
   }
 }
 
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-	txt : 'default text here'
-}
 ReactDOM.render(
-	<App cat={5} txt="this is the props text" />,
+	<App />,
 	document.getElementById('app')
 );
