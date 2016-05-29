a0be0c179e156d82fcf362963a3e2223c3290b77
fawwazmuhammad <fawwazmuhammad@gmail.com>
2016-05-29 16:20:29 +0700

    step 04: Introduction to properies

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Class component can have state
 class App extends React.Component {
   render(){
+  	let txt = this.props.txt
     return (
     	<div>
-    		<h1> Hello World</h1>
-    		<b> Bold </b>
+    		<h1> {this.props.txt}</h1>
+    		<h1> {txt}</h1>
     	</div>
     );
   }
 }
 
-export default App
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt : 'default text here'
+}
+ReactDOM.render(
+	<App cat={5} txt="this is the props text" />,
+	document.getElementById('app')
+);
